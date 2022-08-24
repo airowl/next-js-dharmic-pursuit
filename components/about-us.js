af0e4b5b749bc740800d9f33f77cf98af60c9885
rowl <airowl.gasga@gmail.com>
2022-08-24 17:39:07 +0200

    implement responsive section about us

--- components/about-us.js
+++ components/about-us.js
@@ -23,11 +23,30 @@ const shapeDatas = [
     },
 ];
 
+const shapeDatasMd = [
+    {
+        integer: '18',
+        text: 'Years Experience'
+    },
+    {
+        integer: '240+',
+        text: 'Completed Projects'
+    },
+    {
+        integer: '9.5/10',
+        text: 'Average rating'
+    },
+    {
+        integer: '150+',
+        text: 'Served'
+    },
+];
+
 export default function AboutUs(){
     return(
         <section id="about-us" className="w-screen md:h-screen md:flex md:flex-row-reverse md:grow md:items-center pb-24">
             <div className="shape-a md:w-1/2 overflow-hidden relative w-full">
-                <div className="shape-b flex items-center overflow-hidden">
+                <div className="shape-b flex items-center overflow-hidden md:hidden">
                     {
                         shapeDatas.map((e, i) => (
                             <div className="shape bg-second/[.2] text-second flex flex-col justify-center my-10 mx-8" key={i}>
@@ -41,6 +60,18 @@ export default function AboutUs(){
                         ))
                     }
                 </div>
+                {
+                    shapeDatasMd.map((e, i) => (
+                        <div className="shape bg-second/[.2] text-second flex-col justify-center my-10 mx-8 hidden md:flex md:flex-col" key={i}>
+                            <h2 className="text-4xl">
+                                {e.integer}
+                            </h2>
+                            <p className="text-second">
+                                {e.text}
+                            </p>
+                        </div>
+                    ))
+                }
             </div>
             <div className="px-10 mt-10 md:w-1/2">
                 <h3 className="text-base md:text-2xl text-second text-center md:text-left">
