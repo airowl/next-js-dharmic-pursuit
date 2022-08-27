d8145ecd787d50c2bf6921b05953090411ce438e
rowl <airowl.gasga@gmail.com>
2022-08-27 21:58:10 +0200

    fix bug image in header section

--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ export default function Header(){
             <nav className="bg-transparent border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="#" className="flex items-center">
-                        <Image src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> 
+                        {/*<Image src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Dharmic Pursut</span>
                     </a>
                     <div className="flex items-center lg:order-2">
