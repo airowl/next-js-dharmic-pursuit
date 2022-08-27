db0e9a3cd10a4ca587252a748ea669457382df5a
rowl <airowl.gasga@gmail.com>
2022-08-27 22:04:31 +0200

    change color in header section

--- components/header.js
+++ components/header.js
@@ -22,9 +22,9 @@ export default function Header(){
                         </button>
                     </div>
                     <div className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 overflow-hidden" id="mobile-menu-2">
-                        <ul className={`flex flex-col font-medium transition-all duration-300 ease-in-out lg:flex-row lg:space-x-8 lg:mt-0 ${navbar ? "mt-4" : "mt-[-17rem]"}`}>
+                        <ul className={`flex flex-col font-medium transition-all duration-300 ease-in-out bg-white md:bg-transparent lg:flex-row lg:space-x-8 lg:mt-0 ${navbar ? "mt-4" : "mt-[-17rem]"}`}>
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                                <a href="#" className="block py-2 pr-4 pl-3 text-black rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-black" aria-current="page">Home</a>
                             </li>
                             <li>
                                 <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
