f5a8a5054ab2c642e6ec41b26132e75a5c45261c
Dave Cross <dharmicpursuit@outlook.com>
2022-09-10 20:30:02 +0200

    Changed Brand Name in component/header.js

--- components/header.js
+++ components/header.js
@@ -13,7 +13,7 @@ export default function Header() {
                         <a className="flex items-center">
                             {/*<Image src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                                Dharmic Pursut
+                                Dharmic Pursuit
                             </span>
                         </a>
                     </Link>
