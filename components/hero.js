741c0a80d2c9c7b5e36b858506e0e169507ed986
Dave Cross <dharmicpursuit@outlook.com>
2022-09-14 16:15:43 +0200

    Changes in hero.js line 7 & 10 (text is bigger and better for phone)

--- components/hero.js
+++ components/hero.js
@@ -4,10 +4,10 @@ export default function Hero(){
     return (
         <section id="hero" className="bg-gradient-to-r from-main to-second text-white w-screen h-screen flex items-center">
             <div className="w-full text-center md:text-left md:ml-12 md:w-3/5">
-                <h1 className="text-5xl md:text-7xl font-bold">
+                <h1 className="text-7xl md:text-7xl font-bold">
                     We Envision Financial Wellness
                 </h1>
-                <p className="text-base my-5 md:my-14">
+                <p className="text-xl my-5 md:my-14">
                     Guiding You In Value Investing And Personal Finance
                 </p>
                 <Link href="#">
