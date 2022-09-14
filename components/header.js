fed15eaaf8903856b8f3dca949e55082c4265f5b
Dave Cross <dharmicpursuit@outlook.com>
2022-09-14 16:22:48 +0200

    Change Logo text to white

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ export default function Header() {
                     <Link href="/">
                         <a className="flex items-center">
                             {/*<Image src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
                                 Dharmic Pursuit
                             </span>
                         </a>
