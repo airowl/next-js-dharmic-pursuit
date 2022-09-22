117bf2b8dc6e5b7210e6a6e6b28adabce3ca660c
rowl <airowl.gasga@gmail.com>
2022-09-22 15:12:22 +0200

    fix bug posts json

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home({ posts }) {
         <AboutUs />
         <Tools />
         <Goals />
-        <Blogs posts={posts}/>
+        <Blogs posts={posts ? posts : null}/>
       
     </Layout>
   )
